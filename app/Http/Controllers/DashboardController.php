1c91c20f066bac0e5f9c5e1b902ac5ef63f69754
t.nagata <>
2025-04-30 18:00:29 +0900

    first commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display the user's profile form.
+     */
+    public function index(Request $request): Response
+    {
+        return Inertia::render('Dashboard', []);
+    }
+}
